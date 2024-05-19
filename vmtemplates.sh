a1099a60474ea970b6bf944fc93fd9f003b69fd9
Whogivsachit <evanm6257@gmail.com>
2024-05-19 15:01:29 -0700

    Fix dead links

--- vmtemplates.sh
+++ vmtemplates.sh
@@ -4,34 +4,34 @@
 
 rm vzdump-qemu-*
 
-wget https://cdn.chit.sh/files/vzdump-qemu-100000.vma.zst
+wget https://files.chit.sh/vzdump-qemu-100000.vma.zst
 qmrestore vzdump-qemu-100000.vma.zst 100000 --storage local
 
-wget https://cdn.chit.sh/files/vzdump-qemu-100001.vma.zst
+wget https://files.chit.sh/vzdump-qemu-100001.vma.zst
 qmrestore vzdump-qemu-100001.vma.zst 100001 --storage local
 
-wget https://cdn.chit.sh/files/vzdump-qemu-100002.vma.zst
+wget https://files.chit.sh/vzdump-qemu-100002.vma.zst
 qmrestore vzdump-qemu-100002.vma.zst 100002 --storage local
 
-wget https://cdn.chit.sh/files/vzdump-qemu-110000.vma.zst
+wget https://files.chit.sh/vzdump-qemu-110000.vma.zst
 qmrestore vzdump-qemu-110000.vma.zst 110000 --storage local
 
-wget https://cdn.chit.sh/files/vzdump-qemu-120000.vma.zst
+wget https://files.chit.sh/vzdump-qemu-120000.vma.zst
 qmrestore vzdump-qemu-120000.vma.zst 120000 --storage local
 
-wget https://cdn.chit.sh/files/vzdump-qemu-130000.vma.zst
+wget https://files.chit.sh/vzdump-qemu-130000.vma.zst
 qmrestore vzdump-qemu-130000.vma.zst 130000 --storage local
 
-wget https://cdn.chit.sh/files/vzdump-qemu-140000.vma.zst
+wget https://files.chit.sh/vzdump-qemu-140000.vma.zst
 qmrestore vzdump-qemu-140000.vma.zst 140000 --storage local
 
-wget https://cdn.chit.sh/files/vzdump-qemu-150000.vma.zst
+wget https://files.chit.sh/vzdump-qemu-150000.vma.zst
 qmrestore vzdump-qemu-150000.vma.zst 150000 --storage local
 
-wget https://cdn.chit.sh/files/vzdump-qemu-150001.vma.zst
+wget https://files.chit.sh/vzdump-qemu-150001.vma.zst
 qmrestore vzdump-qemu-150001.vma.zst 150001 --storage local
 
-wget https://cdn.chit.sh/files/vzdump-qemu-150002.vma.zst
+wget https://files.chit.sh/vzdump-qemu-150002.vma.zst
 qmrestore vzdump-qemu-150002.vma.zst 150002 --storage local
 
 # Remove Files
